f68fbff0a81980959bdd2fd229dd343815f002d9
Natsir <hentihu.natsir@gmail.com>
2025-09-18 14:16:15 +0700

    fix: button style

--- src/pages/Divisi.jsx
+++ src/pages/Divisi.jsx
@@ -166,7 +166,6 @@ export default function Divisi() {
                     >
                       Edit
                     </button>
-                    {" | "}
                     <button
                       onClick={() => removeRow(r)}
                       className=" px-4 py-2 rounded-lg bg-red-600 hover:bg-blue-700 text-white"
